c5ded3ffcb446e23423fec8bf64c176e9d363f2e
alex0127 <alex0127@gmail.com>
2017-04-24 16:11:43 +0800

    edit reply msg

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ import (
 	"os"
 	"net/url"
 	"io/ioutil"
-	
+
 	"github.com/line/line-bot-sdk-go/linebot"
 )
 
@@ -62,7 +62,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		if event.Type == linebot.EventTypeMessage {
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
-				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(event.ReplyToken+"---"+message.Text+" OK!")).Do(); err != nil {
+				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.id+"---"+message.Text+" OK!")).Do(); err != nil {
 					log.Print(err)
 				}
 			}
