7babf0b3590c3a4420093d1b9dfccd03e10457c8
alex0127 <alex0127@gmail.com>
2017-04-27 16:01:29 +0800

    123

--- main.go
+++ main.go
@@ -64,13 +64,13 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			case *linebot.TextMessage:
 				//if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(event.ReplyToken+":"+message.ID+"-"+message.Text+" OK!")).Do(); err != nil {
 				if message.Text == "熱銷"{
-					msg = "勤耕延吉 · 電話：(02)25705777 · 地址：台北市松山區光復南路58巷 http://www.hiyes.tw/allcase/yanji/index.html "					
-					msg = msg + "幸福莊園 · 電話：02-2678-7222 · 地址：新北市鶯歌區鳳福路及鳳鳴路口 · 接待地址：新北市鶯歌區鶯歌路及鳳鳴路口 http://www.hiyes.tw/allcase/happymanor/index.html "
+					msg = "http://www.hiyes.tw/allcase/yanji/index.html 勤耕延吉 · 電話：(02)25705777 · 地址：台北市松山區光復南路58巷 "					
+					msg = msg + "http://www.hiyes.tw/allcase/happymanor/index.html 幸福莊園 · 電話：02-2678-7222 · 地址：新北市鶯歌區鳳福路及鳳鳴路口 · 接待地址：新北市鶯歌區鶯歌路及鳳鳴路口  "
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(msg)).Do(); err != nil {
 						log.Print(err)
 					}
 				}
-				if message.Text == "預約"{					
+				if message.Text == "預約"{
 					if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("您想預約的日期")).Do(); err != nil {
 						log.Print(err)
 					}					
